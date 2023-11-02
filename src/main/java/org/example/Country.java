24eb4186b7f5c7dbb48f1e098e931a730f0315b9
Bruno Apolinário <bruno.apolinario010@gmail.com>
2023-11-02 12:55:50 -0300

    fix: constructor, feat: setPopulation, refactor: isNeighbor

--- src/main/java/org/example/Country.java
+++ src/main/java/org/example/Country.java
@@ -5,31 +5,37 @@ public class Country {
     private String name;
     private int population;
     private double dimensionKm2;
-    private Country[] neighbors = new Country[40];
+    private Country[] neighbors;
 
-    public Country(String isoCode, String name, int population, double dimensionKm2) {
+    public Country(String isoCode, String name, double dimensionKm2) {
         this.isoCode = isoCode.toUpperCase();
         this.name = name;
-        this.population = population;
         this.dimensionKm2 = dimensionKm2;
+        this.neighbors = new Country[40];
     }
 
     public String getIsoCode() {
         return this.isoCode;
     }
+
+    public String getName() { return this.name; }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
     public boolean countryExists(Country country) {
-        return this.isoCode.equals(country.getIsoCode());
+        return this.getIsoCode().equals(country.getIsoCode());
     }
     public double getPopulationDensity() {
         return this.population / this.dimensionKm2;
     }
-    public void getNeighbors() {
-        System.out.println("Neighbors of " + this.name + ":");
-        for(Country neighbor : this.neighbors) {
-            if (neighbor != null) {
-                System.out.println(neighbor.getIsoCode());
+    public boolean isNeighbor(Country country) {
+        for (Country neighbor : this.neighbors) {
+            if(neighbor != null && neighbor.countryExists(country)) {
+                return true;
             }
         }
+        return false;
     }
     public void addNeighbor(Country country) {
         for (int i = 0; i < this.neighbors.length; i++) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Country c1 = new Country("BRA", "Brazil", 209500000, 8515767);
-        Country c2 = new Country("ARG", "Argentina", 44270000, 2780400);
-        Country c3 = new Country("PAR", "Paraguay", 7053000, 406752);
+        Country c1 = new Country("BRA", "Brazil", 8515767);
+        Country c2 = new Country("ARG", "Argentina", 2780400);
+        Country c3 = new Country("PAR", "Paraguay", 406752);
+        Country c4 = new Country("FRA", "France", 591675);
 
         c1.addNeighbor(c2);
         c1.addNeighbor(c3);
+        c1.setPopulation(220000000);
         System.out.println(c1.getIsoCode());
+        System.out.println(c1.getName());
         System.out.println(c1.getPopulationDensity());
-        c1.getNeighbors();
+        System.out.println("Faz fronteira com: " + c2.getName() + " " + c1.isNeighbor(c2));
+        System.out.println("Faz fronteira com: " + c4.getName() + " " + c1.isNeighbor(c4));
     }
 }
